37181f4df50cb314bdfce90a1e8a0eb779d2273e
Eduardo <57434811+NoobToDaMaximum@users.noreply.github.com>
2021-10-07 13:56:58 +0200

    Practica entera
    hola

---
+++ multMatrix/mainClienteMultMatrix.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "multmatrix_imp.h"
+
+int main (int argc, char** argv){
+	multmatrix_stub* mulMatrix = new multmatrix_stub();
+}
