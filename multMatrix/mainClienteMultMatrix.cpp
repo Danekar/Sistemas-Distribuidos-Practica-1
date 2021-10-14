2825eda0c7c0ba63b7e78e9d0a55127084b62539
Eduardo <57434811+NoobToDaMaximum@users.noreply.github.com>
2021-10-14 13:21:02 +0200

    Update mainClienteMultMatrix.cpp

--- multMatrix/mainClienteMultMatrix.cpp
+++ multMatrix/mainClienteMultMatrix.cpp
@@ -5,15 +5,15 @@
 
 int main (int argc, char** argv){
 
-	multMatrix_stub mulMatrix = new multMatrix_stub();
+	multMatrix_stub* mulMatrix = new multMatrix_stub();
 
-    matrix_t* m1= mulMatrix.createRandMatrix(5,5);
-    matrix_t* m2= mulMatrix.createIdentity(5,5);
-    matrix_t* mres=mulMatrix.multMatrices(m1,m2);
-    mulMatrix.writeMatrix(mres,"resultado.txt");
-    matrix_t* m3=mulMatrix.readMatrix("resultado.txt");
-    matrix_t* mres2=mulMatrix.multMatrices(m1,m3);
-    mulMatrix.writeMatrix(mres2,"resultado2.txt");
+    matrix_t* m1= mulMatrix->createRandMatrix(5,5);
+    matrix_t* m2= mulMatrix->createIdentity(5,5);
+    matrix_t* mres=mulMatrix->multMatrices(m1,m2);
+    mulMatrix->writeMatrix(mres,"resultado.txt");
+    matrix_t* m3=mulMatrix->readMatrix("resultado.txt");
+    matrix_t* mres2=mulMatrix->multMatrices(m1,m3);
+    mulMatrix->writeMatrix(mres2,"resultado2.txt");
 
 
 
